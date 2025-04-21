3147388c4bd25cc978e1b7b53304c3ce9db51544
Rich Colbert <recolbert@gmail.com>
2025-04-20 22:24:35 -0400

    Initial commit for solo class experiment

---
+++ ClassPlaying/person.py
@@ -0,0 +1,30 @@
+from calendar import weekday
+import datetime
+from math import e
+
+
+
+class person:
+    def __init__(self, name, DOB, gender):
+
+        self.DOB = datetime.datetime.strptime(DOB, "%m-%d-%Y").date()
+        self.name = str(name)
+        self.gender = gender
+
+    def __str__(self):
+        return f"{self.name} was born {self.DOB} and is {self.gender}"
+    
+ 
+    
+rich = person("Rich", "10-13-1982", "Male")
+
+print(rich)
+
+
+print(dir(datetime.date))
+
+if datetime.date.today() == datetime.date.today().weekday():
+    print("Its a weekday")
+
+else:
+    print("Its a weekend")
